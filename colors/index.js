3e188c8dc998a19062eedadc3e64954820c4a67b
bip production <119274726+bipproduction@users.noreply.github.com>
2025-06-19 12:07:12 +0800

    Update index.js

--- colors/index.js
+++ colors/index.js
@@ -1,48 +1,65 @@
 const colors = {
   // Foreground colors
-  black: str => `\x1B[30m${str}\x1B[0m`,
-  red: str => `\x1B[31m${str}\x1B[0m`,
-  green: str => `\x1B[32m${str}\x1B[0m`,
-  yellow: str => `\x1B[33m${str}\x1B[0m`,
-  blue: str => `\x1B[34m${str}\x1B[0m`,
-  magenta: str => `\x1B[35m${str}\x1B[0m`,
-  cyan: str => `\x1B[36m${str}\x1B[0m`,
-  white: str => `\x1B[37m${str}\x1B[0m`,
-  gray: str => `\x1B[90m${str}\x1B[0m`,
+  black: str => `\x1b[30m${str}\x1b[0m`,
+  red: str => `\x1b[31m${str}\x1b[0m`,
+  green: str => `\x1b[32m${str}\x1b[0m`, // Fixed syntax error
+  yellow: str => `\x1b[33m${str}\x1b[0m`,
+  blue: str => `\x1b[34m${str}\x1b[0m`,
+  magenta: str => `\x1b[35m${str}\x1b[0m`,
+  cyan: str => `\x1b[36m${str}\x1b[0m`,
+  white: str => `\x1b[37m${str}\x1b[0m`,
+  gray: str => `\x1b[90m${str}\x1b[0m`,
 
   // Background colors
-  bgBlack: str => `\x1B[40m${str}\x1B[0m`,
-  bgRed: str => `\x1B[41m${str}\x1B[0m`,
-  bgGreen: str => `\x1B[42m${str}\x1B[0m`,
-  bgYellow: str => `\x1B[43m${str}\x1B[0m`,
-  bgBlue: str => `\x1B[44m${str}\x1B[0m`,
-  bgMagenta: str => `\x1B[45m${str}\x1B[0m`,
-  bgCyan: str => `\x1B[46m${str}\x1B[0m`,
-  bgWhite: str => `\x1B[47m${str}\x1B[0m`,
+  bgBlack: str => `\x1b[40m${str}\x1b[0m`,
+  bgRed: str => `\x1b[41m${str}\x1b[0m`,
+  bgGreen: str => `\x1b[42m${str}\x1b[0m`,
+  bgYellow: str => `\x1b[43m${str}\x1b[0m`,
+  bgBlue: str => `\x1b[44m${str}\x1b[0m`,
+  bgMagenta: str => `\x1b[45m${str}\x1b[0m`,
+  bgCyan: str => `\x1b[46m${str}\x1b[0m`,
+  bgWhite: str => `\x1b[47m${str}\x1b[0m`,
 
   // Text styles
-  reset: str => `\x1B[0m${str}\x1B[0m`,
-  bold: str => `\x1B[1m${str}\x1B[0m`,
-  dim: str => `\x1B[2m${str}\x1B[0m`,
-  italic: str => `\x1B[3m${str}\x1B[0m`,
-  underline: str => `\x1B[4m${str}\x1B[0m`,
-  inverse: str => `\x1B[7m${str}\x1B[0m`,
-  hidden: str => `\x1B[8m${str}\x1B[0m`,
-  strikethrough: str => `\x1B[9m${str}\x1B[0m`,
+  reset: str => `\x1b[0m${str}\x1b[0m`,
+  bold: str => `\x1b[1m${str}\x1b[0m`,
+  dim: str => `\x1b[2m${str}\x1b[0m`,
+  italic: str => `\x1b[3m${str}\x1b[0m`,
+  underline: str => `\x1b[4m${str}\x1b[0m`,
+  inverse: str => `\x1b[7m${str}\x1b[0m`,
+  hidden: str => `\x1b[8m${str}\x1b[0m`,
+  strikethrough: str => `\x1b[9m${str}\x1b[0m`,
 
   // Additional colors (approximations or aliases)
-  grey: str => `\x1B[90m${str}\x1B[0m`, // Alias for gray
-  brightRed: str => `\x1B[91m${str}\x1B[0m`,
-  brightGreen: str => `\x1B[92m${str}\x1B[0m`,
-  brightYellow: str => `\x1B[93m${str}\x1B[0m`,
-  brightBlue: str => `\x1B[94m${str}\x1B[0m`,
-  brightMagenta: str => `\x1B[95m${str}\x1B[0m`,
-  brightCyan: str => `\x1B[96m${str}\x1B[0m`,
-  brightWhite: str => `\x1B[97m${str}\x1B[0m`
+  grey: str => `\x1b[90m${str}\x1b[0m`, // Alias for gray
+  brightRed: str => `\x1b[91m${str}\x1b[0m`,
+  brightGreen: str => `\x1b[92m${str}\x1b[0m`,
+  brightYellow: str => `\x1b[93m${str}\x1b[0m`,
+  brightBlue: str => `\x1b[94m${str}\x1b[0m`,
+  brightMagenta: str => `\x1b[95m${str}\x1b[0m`,
+  brightCyan: str => `\x1b[96m${str}\x1b[0m`,
+  brightWhite: str => `\x1b[97m${str}\x1b[0m`
 };
 
-if (typeof window !== "undefined") {
-  window.colors = colors;
-} else if (typeof module !== "undefined" && module.exports) {
-  module.exports = colors;
-} 
+// Add color and style methods to String.prototype
+Object.keys(colors).forEach(key => {
+  Object.defineProperty(String.prototype, key, {
+    configurable: true,
+    enumerable: false, // Changed to false to avoid polluting for...in loops
+    get: function () {
+      return colors[key](this.toString());
+    }
+  });
+});
+
+// Export for different environments
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = colors; // CommonJS (Node.js)
+} else if (typeof window !== 'undefined') {
+  window.colors = colors; // Browser global
+}
+
+// Support ES modules if needed
+if (typeof exportDefault !== 'undefined') {
+  exportDefault = colors;
+}
